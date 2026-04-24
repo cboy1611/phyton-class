1b8befce5bfea296949b57affb38a9bcacb78bc5
cboy1611 <akabs2012@gmail.com>
2026-04-24 12:36:48 +0100

    sixth commit

---
+++ loops/reverse a string.py
@@ -0,0 +1,8 @@
+string = input("please enter a string to reverse: ")
+string2 = ("")
+
+for i in string:
+    string2 = i + string2
+
+print ("\nthe original string is: ", string)
+print ("\nthe reversed string is: ", string2)
